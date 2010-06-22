74d13ad5514bb3333b371e7a793fed9ee1dd0d77
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2010-06-22 13:29:19 +0000

    documentation of controller

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,10 @@ settings.configure()
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.inheritance_diagram']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.autosummary', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -204,3 +207,5 @@ intersphinx_mapping = {'http://docs.python.org/': None,
                        'http://docs.djangoproject.com/en/dev': 'http://docs.djangoproject.com/en/dev/_objects',}
 
 autodoc_member_order = 'groupwise'
+
+autosummary_genererate = ["models.rst", "controllers.rst"]
