52e61a03aa4623889fd1d617dfa6c15c897869c8
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2010-07-30 13:35:27 +0000

    ;-)

---
+++ trunk/openPLM/pour_forms.py
@@ -0,0 +1,14 @@
+class FakeItems(object):
+    def __init__(self, values):
+        self.values = values
+    
+    def items(self):
+        return self.values
+
+# example
+Form = type("ppp", (forms.BaseForm,), {"base_fields" : FakeItems( [("e", forms.CharField()), ("a", forms.CharField()), ("t", forms.IntegerField())] )})
+
+# possible values from a list of field names
+values = [(name, fields_dict[name]) for name in sorted_list]
+
+
