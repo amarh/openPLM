d3b4285e85b98f106615b9cd7332ae8f7f8e9db5
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2010-08-18 09:42:43 +0000

    tests : split tests.py in several files

---
+++ trunk/openPLM/plmapp/tests/__init__.py
@@ -0,0 +1,17 @@
+from openPLM.plmapp.tests.controllers import *
+from openPLM.plmapp.tests.lifecycle import *
+from openPLM.plmapp.tests.views import *
+
+def get_doctest(module_name):
+    test_dict={}
+    module = __import__(module_name,{},{},module_name.split('.')[-1])
+    for obj_name,obj in module.__dict__.items():
+        if '__module__' in dir(obj) and obj.__module__ == module_name:
+            if obj.__doc__:
+                test_dict[obj_name] = obj.__doc__
+                return test_dict
+
+__test__ = get_doctest("plmapp.utils")
+__test__.update(get_doctest("plmapp.controllers"))
+__test__.update(get_doctest("plmapp.lifecycle"))
+
