0ee2e7f51136157904f0d87112e6c899d11ff001
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2010-08-26 13:40:28 +0000

    add a settings file for tests

---
+++ openPLM/settings_tests.py
@@ -0,0 +1,119 @@
+# Django settings for openPLM project.
+# sqlite version
+
+import os.path
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+    ("pcosquer", "pierre.cosquer@insa-rennes.fr"),
+    ("pjoulaud", "ninoo.fr@gmail.com"),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'database.db'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Paris'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-en'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = 'media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '0ham7d#fh669-xi@wxf1wcpbhn6tbbegtv_cml()_wcboyw&u&'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'openPLM.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    "templates",
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'openPLM.plmapp',
+    # you can add your application after this line
+    'openPLM.cad',
+    'openPLM.computer',
+    'openPLM.cae',
+    'openPLM.office',
+)
+
+AUTH_PROFILE_MODULE = 'plmapp.UserProfile'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 6666
+
+######################
+# openPLM's settings #
+######################
+
+#: directory that stores documents
+DOCUMENTS_DIR = "/tmp"
+THUMBNAILS_DIR = os.path.join(MEDIA_ROOT, "thumbnails")
+#: directory that stores thumbnails
+THUMBNAILS_URL = MEDIA_URL + "thumbnails"
+
+# Cookie used for session is temporary and is deleted when browser is closed
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# Add user, messages and perms variables in RequestContext
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+)
+
+#: expeditor's mail used when sending notification emails
+EMAIL_OPENPLM = "no-reply@openplm.example.com",
+
