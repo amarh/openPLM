c456cff0d53cc962bb39a4ffe518fbf820f1165e
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2010-08-26 15:14:54 +0000

    howto django app

---
+++ docs/code/bicycle/tests.py
@@ -0,0 +1,36 @@
+from openPLM.plmapp.tests.controllers import PartControllerTest
+from openPLM.plmapp.tests.views import ViewTest
+
+from openPLM.bicycle.models import BicycleController
+
+class BicycleControllerTest(PartControllerTest):
+    TYPE = "Bicycle"
+    CONTROLLER = BicycleController
+    DATA = {"nb_wheels" : 2}
+
+
+class BicycleViewTest(ViewTest):
+    TYPE = "Bicycle"
+    DATA = {"nb_wheels" : 2,
+            "color" : "blue"}
+    
+    # our custom attributes view adds the ":" so, we redefine this test
+    def test_display_attributes(self):
+        response = self.client.get(self.base_url + "attributes/")
+        self.assertEqual(response.status_code,  200)
+        self.failUnless(response.context["object_attributes"])
+        attributes = dict(response.context["object_attributes"])
+        # name : empty value
+        self.assertEqual(attributes["name:"], "")
+        # owner and creator : self.user
+        self.assertEqual(attributes["owner:"], self.user)
+        self.assertEqual(attributes["creator:"], self.user)    
+    
+    def test_display_attributes2(self):
+        response = self.client.get(self.base_url + "attributes/")
+        self.assertEqual(response.status_code, 200)
+        self.failUnless(response.context["object_attributes"])
+        attributes = dict(response.context["object_attributes"])
+        self.assertEqual(attributes["Number of wheels:"], self.DATA["nb_wheels"])
+        self.assertEqual(attributes["color:"], self.DATA["color"])
+        self.assertEqual(attributes["details:"], "")
