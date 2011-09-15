5c185a667c2fd981166e5582769ad3fba2ee1d73
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-09-15 12:50:50 +0000

    navigate: add a preview of documents

--- openPLM/media/js/navigate.js
+++ openPLM/media/js/navigate.js
@@ -1,50 +1,101 @@
+
+function show_thumbnails_panel(node){
+    if ($("#navThumbnails").is(":hidden")) {
+        $("#navThumbnails").show();
+        $("#FilterNav").css("right", "190px");
+        var width = node.width();
+        if (node.offset().left + width > $("#navThumbnails").offset().left){
+            var left = $("#DivNav").position().left - 180;
+            $("#DivNav").css("left", left);
+        }
+    }
+}
+
+function hide_thumbnails_panel(){
+    $("#navThumbnails").hide();
+    $("#FilterNav").css("right", "10px");
+}
+
+function display_thumbnails(node_id, ajax_url){
+    $("#thumbnailsList").empty();
+    $.ajax({
+        url: ajax_url,
+        success: function( data ) {
+            $.each(data["files"], function(index, value){
+                var name = value[0];
+                var url = value[1];
+                var img = value[2];
+                var title = "Download " + name;
+                $("#thumbnailsList").append(
+                    $("<a href='"+ url +"'> <img src='" + img + "' width='160' height='160' title='" + title + "' /></a>"));
+                });
+            $("#navDocument").text(data["doc"]);
+            show_thumbnails_panel($("#" + node_id));
+        }
+    });
+}
+
 $(document).ready(function(){
 
-  // Supprime la scrollbar en JS
-  $('#Navigate').css('overflow', 'hidden');
+        // Supprime la scrollbar en JS
+        $('#Navigate').css('overflow', 'hidden');
 
-  // Insert les images de navigation
-//    $('#Navigate')
-//    .append('<div id="imgManagement"><span class="imgManagement" id="topControl"></span><span class="imgManagement" id="leftControl"></span><span class="imgManagement" id="rightControl"></span><span class="imgManagement" id="bottomControl"></span></div>');
+        // Insert les images de navigation
+        //    $('#Navigate')
+        //    .append('<div id="imgManagement"><span class="imgManagement" id="topControl"></span><span class="imgManagement" id="leftControl"></span><span class="imgManagement" id="rightControl"></span><span class="imgManagement" id="bottomControl"></span></div>');
 
-  // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #rightControl
-  $('#rightControl')
-    .bind('click', function(){
-      // Move slideInner using left attribute for position
-      $('#DivNav').animate({
-        "left": "-=100px"
-      }, "fast");
-    });
-    
-  // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #leftControl
-  $('#leftControl')
-    .bind('click', function(){
-      // Move slideInner using left attribute for position
-      $('#DivNav').animate({
-        "left": "+=100px"
-      }, "fast");
-    });
+        // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #rightControl
+        $('#rightControl')
+        .bind('click', function(){
+            // Move slideInner using left attribute for position
+            $('#DivNav').animate({
+                "left": "-=100px"
+                }, "fast");
+            });
 
-  // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #topControl
-  $('#topControl')
-    .bind('click', function(){
-      // Move slideInner using top attribute for position
-      $('#DivNav').animate({
-        "top": "+=100px"
-      }, "fast");
-    });
+        // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #leftControl
+        $('#leftControl')
+        .bind('click', function(){
+            // Move slideInner using left attribute for position
+            $('#DivNav').animate({
+                "left": "+=100px"
+                }, "fast");
+            });
 
-  // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #bottomControl
-  $('#bottomControl')
-    .bind('click', function(){
-      // Move slideInner using left attribute for position
-      $('#DivNav').animate({
-        "top": "-=100px"
-      }, "fast");
-    });
+        // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #topControl
+        $('#topControl')
+            .bind('click', function(){
+                    // Move slideInner using top attribute for position
+                    $('#DivNav').animate({
+                        "top": "+=100px"
+                        }, "fast");
+                    });
 
-  $("#DivNav").draggable({
-      cursor: 'crosshair'
-      });
+        // crée un écouteur pour l'évènement de type clic sur les div qui ont l' id #bottomControl
+        $('#bottomControl')
+            .bind('click', function(){
+                    // Move slideInner using left attribute for position
+                    $('#DivNav').animate({
+                        "top": "-=100px"
+                        }, "fast");
+                    });
 
-  });
+        $("#DivNav").draggable({
+cursor: 'crosshair'
+});
+        $("div.node").mouseenter(
+        function () {
+            $(this).find(".node_thumbnails").show();
+        }); 
+        $("div.node").mouseleave(
+        function () {
+            $(this).find(".node_thumbnails").hide();
+        }
+        );
+
+        $("#closeThumbnails").button({
+            icons : {
+                primary: 'ui-icon-close'
+                },
+            text: false}).click(hide_thumbnails_panel);
+});
