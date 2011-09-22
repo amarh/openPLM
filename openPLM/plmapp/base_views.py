d6b9a11fcdccd12c17628c7d231f41ab42201e76
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-09-22 10:29:20 +0000

    add licence

--- openPLM/plmapp/base_views.py
+++ openPLM/plmapp/base_views.py
@@ -1,3 +1,30 @@
+#-!- coding:utf-8 -!-
+
+############################################################################
+# openPLM - open source PLM
+# Copyright 2010 Philippe Joulaud, Pierre Cosquer
+# 
+# This file is part of openPLM.
+#
+#    openPLM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    openPLM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact :
+#    Philippe Joulaud : ninoo.fr@gmail.com
+#    Pierre Cosquer : pierre.cosquer@insa-rennes.fr
+################################################################################
+
+
 import re
 import datetime
 from functools import wraps
