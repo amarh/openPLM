b414ce6b7fbe0b0596a8c5f39487f8eb3e90495e
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-09-27 09:19:00 +0000

    move MetaController and get_controller to a new module

--- openPLM/plmapp/controllers/plmobject.py
+++ openPLM/plmapp/controllers/plmobject.py
@@ -36,57 +36,7 @@ from openPLM.plmapp.mail import send_mail
 from openPLM.plmapp.exceptions import RevisionError, PermissionError,\
     PromotionError
 from openPLM.plmapp.utils import level_to_sign_str
-
-_controller_rx = re.compile(r"(?P<type>\w+)Controller")
-
-class MetaController(type):
-    """
-    Metaclass used to register a controller and get a controller associated
-    to a type (see :meth:`get_controller`).
-
-    See `how-to-add-a-controller`_ for more explanations.
-    """
-    #: dict<type_name(str) : Controller(like :class:`PLMObjectController`)>
-    controllers_dict = {}
-
-    def __new__(mcs, name, bases, attrs):
-        cls = type.__new__(mcs, name, bases, attrs)
-        if "MANAGED_TYPE" in attrs:
-            managed = attrs["MANAGED_TYPE"].__name__
-        else:
-            m = _controller_rx.match(name)
-            if m:
-                managed = m.group("type")
-            else:
-                # the controller is not interresting
-                return cls
-        mcs.controllers_dict[managed] = cls
-        return cls
-
-    @classmethod
-    def get_controller(cls, type_name):
-        """
-        Returns the a controller (subclass of :class:`PLMObjectController`) 
-        associated to *type_name* (a string).
-
-        For example, ``get_controller("Part")`` will return the class 
-        :class:`.PartController`.
-        """
-        if type_name in cls.controllers_dict:
-            return cls.controllers_dict[type_name]
-        else:
-            # get his model and return his parent controller
-            if type_name == "PLMObject":
-                # just a security to prevent an infinite recursion
-                return PLMObjectController
-            else:
-                model = models.get_all_plmobjects()[type_name]
-                parents = [p for p in model.__bases__ 
-                                if issubclass(p, models.PLMObject)]
-                return cls.get_controller(parents[0].__name__)
-
-#: shortcut for :meth:`MetaController.get_controller`
-get_controller = MetaController.get_controller
+from openPLM.plmapp.controllers.base import MetaController
 
 def permission_required(func=None, role="owner"):
     """
