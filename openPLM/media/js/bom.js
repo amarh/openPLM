2d789a4f34f2042f60fe152b65455afb59267971
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-10-11 14:16:55 +0000

    templates/js: BOM: add arrows to expand/collapse nodes

--- openPLM/media/js/bom.js
+++ openPLM/media/js/bom.js
@@ -1,12 +1,55 @@
+function get_level(node){
+
+    var cls = node.attr("class");
+    var level = /level(\d+)/.exec(cls)[1];
+    return Number(level);
+}
+
+function all(array) {
+    var len = array.length;
+    for (var i=0; i < len; i++) {
+        if (! array[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 $(
 function(){
-    $(".expander").click(
-        function() {
-            var cls = $(this).attr("class");
-            var level = /level\d+/.exec(cls)[0];
-        }
-    );
-    
 
-    }
+    $("td.expander").click(function () {
+        var row = $(this);
+        var level = get_level(row);
+        row.toggleClass("expanded open");
+        var exps = new Array();
+        exps.push(row.hasClass("open"));
+        var nodes = row.parent().next().children('.expander');
+        var is_child = nodes.size() != 0;
+        var last_level = level;
+        while (is_child){
+            var node = nodes.first();
+            var l = get_level(node);
+            if (l > level){
+                if (all(exps.slice(0, l-level))){
+                    node.parent().show();
+                }
+                else {
+                    node.parent().hide();
+                }
+                nodes = node.parent().next().children('.expander'); 
+                is_child = nodes.size() != 0;
+                if (l <= last_level){
+                    exps = exps.slice(0, l-level);
+                }
+                exps.push(node.hasClass("open"));
+                last_level = l;
+            }
+            else {
+                is_child = false;
+            }
+        }
+
+    });
+}
 );
