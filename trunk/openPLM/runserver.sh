77c00de73f90d1bdef350e3ab6cc2fb039928206
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-10-26 13:19:56 +0000

    oops

--- trunk/openPLM/runserver.sh
+++ trunk/openPLM/runserver.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-./manage.py celeryd -Q mails,index,celery -c 3 -l info --settings=my_settings &
+./manage.py celeryd -Q mails,index,celery -c 3 -l info &
 python -m smtpd -n -c DebuggingServer localhost:1025 &
 ./manage.py runserver
 exit 0
