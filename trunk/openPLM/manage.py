05b2bb46e712e4f9223321b55830c98c23c66df1
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-11-04 09:19:30 +0000

    manage.py: if an ImportError is raised, print it. Reported by Jason Morgan

--- trunk/openPLM/manage.py
+++ trunk/openPLM/manage.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 from django.core.management import execute_manager
+
 try:
     import settings # Assumed to be in the same directory.
-except ImportError:
+except ImportError as er:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    # print 'er' in case of a missing module dependence
+    # reported by Jason Morgan
+    sys.stderr.write(str(er))
+    sys.stderr.write("\nError: Can't find the file 'settings.py' in the directory\n")
     sys.exit(1)
+
 
 if __name__ == "__main__":
     execute_manager(settings)
