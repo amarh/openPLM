84e76580c0dc1e4e523e51f98b57c0352ae4d239
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-11-10 10:20:23 +0000

    doc: update 'how-to add a new model (application)'

--- trunk/docs/code/bicycle/views.py
+++ trunk/docs/code/bicycle/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
 import openPLM.plmapp.views as pviews
 
 
 def attributes(request, obj_ref, obj_revi):
     """ Manage html page for attributes """
     obj_type = "Bicycle"
-    obj, ctx, request_dict = pviews.get_generic_data(request, obj_type, obj_ref, obj_revi)
+    obj, ctx = pviews.get_generic_data(request, obj_type, obj_ref, obj_revi)
     object_attributes = []
     for attr in obj.attributes:
         item = obj.get_verbose_name(attr) + ":" # <- this is our small modification
         object_attributes.append((item, getattr(obj, attr)))
     ctx.update({'current_page':'attributes', 
                 'object_attributes': object_attributes})
-    request.session.update(request_dict)
-    return render_to_response('DisplayObject.htm', ctx,
-                              context_instance=RequestContext(request))
+    return pviews.r2r('DisplayObject.htm', ctx, request)
 
