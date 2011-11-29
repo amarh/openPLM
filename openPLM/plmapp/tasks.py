7ebf2c3d8b03876018bee2f95ac6d674922b258c
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2011-11-29 12:28:16 +0000

    tasks: update_index and update_indexes use the same lock

--- openPLM/plmapp/tasks.py
+++ openPLM/plmapp/tasks.py
@@ -10,13 +10,12 @@ from haystack import site
 
 from celery.task import task
 
-def synchronized(cls):
+def synchronized(cls=None, lock=None):
     """Class decorator to synchronize execution of a task's run method.
 
     This prevents parallel execution of two instances of the same task within
     the same worker. If an instance of the same task is running in the same
-    worker, the second invocation calls :meth:`~celery.task.base.Task.retry`
-    is called instead of running the task.
+    worker, the second invocation blocks until the first one completes.
 
     Note that this applies to the task class, so `@synchronized` should
     appear before `@task` or `@periodic_task` when tasks are defined with
@@ -31,36 +30,31 @@ def synchronized(cls):
             logger.warn("Task running...")
     """
     from multiprocessing import Lock
-    cls.lock = Lock()
+    cls.lock = lock or Lock()
     cls.unsynchronized_run = cls.run
     @wraps(cls.unsynchronized_run)
     def wrapper(*args, **kwargs):
-        if cls.lock.acquire(False):
-            try:
-                cls.unsynchronized_run(*args, **kwargs)
-            finally:
-                cls.lock.release()
-        else:
-            cls.retry(args=args, kwargs=kwargs)
+        with cls.lock:
+            cls.unsynchronized_run(*args, **kwargs)
     cls.run = wrapper
     return cls
 
 @synchronized
-@task(default_retry_delay = 5 * 60, max_retries = 1)
+@task(default_retry_delay = 60, max_retries = 10)
 def update_index(app_name, model_name, pk, **kwargs):
     model_class = get_model(app_name, model_name)
     instance = model_class.objects.get(pk=pk)
     search_index = site.get_index(model_class)
     search_index.update_object(instance)
 
-@synchronized
-@task(default_retry_delay = 5 * 60, max_retries = 1)
+@task(default_retry_delay = 60, max_retries = 10)
 def update_indexes(instances):
     for app_name, model_name, pk in instances:
         model_class = get_model(app_name, model_name)
         instance = model_class.objects.get(pk=pk)
         search_index = site.get_index(model_class)
         search_index.update_object(instance)
+update_indexes = synchronized(update_indexes, update_index.lock)
 
 @task
 def add(a, b):
