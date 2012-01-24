391843dd3fbd7d5da701f325f40fc2b78bfc15f2
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-01-24 11:02:15 +0000

    gdoc: fix edit url

--- openPLM/gdoc/gutils.py
+++ openPLM/gdoc/gutils.py
@@ -4,7 +4,8 @@ import gdata.docs.client
 def get_gdocs(gclient):
     return [(r.resource_id.text, r.title.text) for r in gclient.get_all_resources()] 
 
-SCOPES = ['https://docs.google.com/feeds/', ] 
+SCOPES = ['https://docs.google.com/feeds/',
+          'https://spreadsheets.google.com/feeds/' ] 
 USER_AGENT = "OpenPLM"
 
 def get_gclient(valid_credential):
