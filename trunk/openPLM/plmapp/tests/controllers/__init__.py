aaced05752fc7424b055820f006568417a2d4220
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-02-23 15:33:17 +0000

    controllers: change some rules:     * a proposed object (not draft, not yet official) is no more editable     * rules to attach a document to a part have changed

--- trunk/openPLM/plmapp/tests/controllers/__init__.py
+++ trunk/openPLM/plmapp/tests/controllers/__init__.py
@@ -3,3 +3,4 @@ from openPLM.plmapp.tests.controllers.plmobject import *
 from openPLM.plmapp.tests.controllers.part import *
 from openPLM.plmapp.tests.controllers.document import *
 from openPLM.plmapp.tests.controllers.group import *
+from openPLM.plmapp.tests.controllers.partdoc import *
