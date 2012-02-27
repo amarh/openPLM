f25ef03c352579c90a99732a270e7d2abbe97614
agalech <agalech@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-02-27 12:56:03 +0000

    add test for gestion document native

--- openPLM/plmapp/tests/__init__.py
+++ openPLM/plmapp/tests/__init__.py
@@ -39,6 +39,7 @@ from openPLM.plmapp.tests.api import *
 from openPLM.plmapp.tests.csvimport import *
 from openPLM.plmapp.tests.archive import *
 from openPLM.plmapp.tests.pcle import *
+from openPLM.plmapp.tests.gestion_document_native import *
 
 import openPLM.plmapp.models
 from openPLM.plmapp.lifecycle import LifecycleList
