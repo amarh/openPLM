e09786fd06aeb0562996d9c08bb182e6baf799bc
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-03-20 14:30:01 +0000

    coverage: gives shell arguements to coverage

--- openPLM/run_coverage.sh
+++ openPLM/run_coverage.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-coverage run ./manage.py test  --settings=settings_tests
-coverage run -a plmapp/utils.py
-coverage run -a plmapp/lifecycle.py
-coverage html
+coverage run $* ./manage.py test  --settings=settings_tests
+coverage run $* -a plmapp/utils.py
+coverage run $* -a plmapp/lifecycle.py
+coverage html $*
+
