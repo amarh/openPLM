f72bb2e7e965907f56f09f78087b30faa5869bd9
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-03-27 11:41:35 +0000

    forms: do not group types of type forms containing only part or document types

--- trunk/openPLM/document3D/forms.py
+++ trunk/openPLM/document3D/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 from openPLM.document3D.models import *
-from django.db.models import Q
-from django.db import models
 from openPLM.plmapp.models import get_all_parts_with_level
-from openPLM.plmapp.forms import group_types
-from django.forms.formsets import formset_factory
 from openPLM.plmapp.units import UNITS
             
 class Doc_Part_type_Form(forms.Form):
-    LIST_parts = group_types(get_all_parts_with_level())
+    LIST_parts = get_all_parts_with_level()
     type_part = forms.TypedChoiceField(choices=LIST_parts,label='')
     type_part.widget.attrs["onchange"]="update_form(this,'doc_show')"
     type_part.widget.attrs["class"]="selector"
     #type_part.widget.attrs["onchange"]="this.options[this.selectedIndex].selected"
 
       
-    LIST_document3D = group_types(get_all_plmDocument3Dtypes_with_level())  
+    LIST_document3D = get_all_plmDocument3Dtypes_with_level()  
     type_document3D = forms.TypedChoiceField(choices=LIST_document3D,label='')
     type_part.widget.attrs["onchange"]="update_form(this,'part_show')"
     
--- trunk/openPLM/plmapp/forms.py
+++ trunk/openPLM/plmapp/forms.py
@@ -224,11 +224,11 @@ class TypeFormWithoutUser(forms.Form):
             label=_("Select a type"))
 
 class PartTypeForm(forms.Form):
-    LIST = group_types(m.get_all_parts_with_level())
+    LIST = m.get_all_parts_with_level()
     type = forms.TypedChoiceField(choices=LIST, label=_("Select a type"))
 
 class DocumentTypeForm(forms.Form):
-    LIST = group_types(m.get_all_documents_with_level())
+    LIST = m.get_all_documents_with_level()
     type = forms.TypedChoiceField(choices=LIST, label=_("Select a type"))
 
 class FakeItems(object):
