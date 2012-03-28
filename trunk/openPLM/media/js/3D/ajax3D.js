a3e205ca05655c0196ebb3e8382575fe09a12b36
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-03-28 13:32:08 +0000

    document3D: decompose view: css

--- trunk/openPLM/media/js/3D/ajax3D.js
+++ trunk/openPLM/media/js/3D/ajax3D.js
@@ -1,15 +1,18 @@
 
 function update_part_form() {
+    var form = $("#decompose_form");
+    form.showLoading();
     var prefix = $(this).attr("id").replace("-type_part", "").replace("id_", "");
-    var params = $("#decompose_form").serialize();
+    var params = form.serialize();
     $.ajax({
         url: "/ajax/decompose/"+prefix + "/?" + params,
         success: function( part_form ){
             $("#extra_part_" + prefix).html(part_form);
-            if ($("#part_show" + prefix.replace("form-", "")).attr("value") == "+" ){
+            if ($("#part_show" + prefix.replace("form-", "")+ ">span").text() == "+" ){
                 $("#part_show" + prefix.replace("form-", "")).click();
             }
             make_combobox();
+            form.hideLoading();
             }
     });
 }
@@ -17,15 +20,15 @@ function update_part_form() {
     
 
 $(function() {
-        $("#decompose_form input.toggle_extra_attributes_button").button()
-            .css({"padding": "0.2em"}).click(
-            function () {
-                var id = $(this).attr("id");
-                $("tr." + id).toggle();
-                $(this).attr("value", ($(this).attr("value") == "+") ? "-" : "+");
-            }
-        );
-        $("#decompose_form td.part_type_form > select").change(update_part_form);
+    $("#decompose_form button.toggle_extra_attributes_button").button().click(
+        function () {
+            var id = $(this).attr("id");
+            $("tr." + id).toggle();
+            $(this).children("span").text($(this).children("span").text() == "+" ? "-" : "+");
+            return false;
         }
+    );
+    $("#decompose_form td.part_type_form > select").change(update_part_form);
+    }
  );
 
