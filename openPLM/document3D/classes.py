d0616faf40770106948717b749a02408bbe02a95
agalech <agalech@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-04-16 13:36:51 +0000

    Decomposition multi-level for document3D

--- openPLM/document3D/classes.py
+++ openPLM/document3D/classes.py
@@ -1,9 +1,16 @@
 from OCC.gp import gp_Trsf
+from OCC.GarbageCollector import garbage
 import hashlib
 import string
 import random
 import os
 import django.utils.simplejson as json
+import sys
+
+def new_collect_object(self, obj_deleted):
+        self._kill_pointed()
+
+garbage.collect_object=new_collect_object
 
 
 def get_available_name(location, name):
@@ -27,7 +34,7 @@ def get_available_name(location, name):
 
 class Product(object):
 
-    __slots__ = ("label_reference","name","doc_id","links","geometry","deep","doc_path")
+    __slots__ = ("label_reference","name","doc_id","links","geometry","deep","doc_path","visited","part_to_decompose")
     
     
     def __init__(self,name,deep,label_reference,doc_id,doc_path=None,geometry=None):
@@ -36,9 +43,11 @@ class Product(object):
         self.label_reference=label_reference
         self.name=name
         self.doc_id=doc_id
-        self.doc_path=doc_path
+        self.doc_path=doc_path # indica tmb si el elemento ya ha sido descompuesto
+        self.part_to_decompose=False
         self.geometry=geometry
         self.deep=deep
+        self.visited=False
     def set_geometry(self,geometry):
         self.geometry=geometry
     
@@ -48,10 +57,15 @@ class Product(object):
         for link in self.links:
             if not link.product.doc_id == self.doc_id:
                 return True 
-        return False    
+        return False
+    @property  
+    def is_assembly(self):
+        if self.links:
+            return self.name 
+        return False 
 class Link(object):
 
-    __slots__ = ("names","locations","product","quantity")
+    __slots__ = ("names","locations","product","quantity","visited")
     
     
     def __init__(self,product):
@@ -60,6 +74,7 @@ class Link(object):
         self.locations=[]
         self.product=product
         self.quantity=0
+        self.visited=False#used only in multi-level decomposition
 
 
     def add_occurrence(self,name,Matrix_rotation):
@@ -76,7 +91,7 @@ class Matrix_rotation(object):
     __slots__ = ("x1","x2","x3","x4","y1","y2","y3","y4","z1","z2","z3","z4")
     
     
-    def __init__(self,transformation,list_coord=False):
+    def __init__(self,list_coord):
     
         if list_coord:
             self.x1=list_coord[0]           
@@ -92,82 +107,120 @@ class Matrix_rotation(object):
             self.z3=list_coord[10] 
             self.z4=list_coord[11]    
 
-        else:   
-
-            m=transformation.VectorialPart()
-            gp=m.Row(1)
-            self.x1=gp.X()           
-            self.x2=gp.Y()
-            self.x3=gp.Z()
-            self.x4=transformation.Transforms()[0]
-            gp=m.Row(2)
-            self.y1=gp.X()          
-            self.y2=gp.Y()
-            self.y3=gp.Z()
-            self.y4=transformation.Transforms()[1]
-            gp=m.Row(3)
-            self.z1=gp.X()         
-            self.z2=gp.Y()
-            self.z3=gp.Z()
-            self.z4=transformation.Transforms()[2]   
+    
     def Transformation(self):
         transformation=gp_Trsf()
         transformation.SetValues(self.x1,self.x2,self.x3,self.x4,self.y1,self.y2,self.y3,self.y4,self.z1,self.z2,self.z3,self.z4,1,1)
         return transformation     
-        
+          
     def to_array(self):    
         return [self.x1,self.x2,self.x3,self.x4,self.y1,self.y2,self.y3,self.y4,self.z1,self.z2,self.z3,self.z4] 
         
 
 
 
-
-
-def generateArbre(arbre,deep=0,product_root=None):   
+def generateArbre(arbre,product=False,product_root=False,deep=0,from_child_ArbreFile=False):   
     label_reference=False
-    product_assembly=search_assembly(arbre[0][0],label_reference,arbre[0][1],product_root)            
-    if product_assembly:
-        return product_assembly 
-    else:    
-        product=Product(arbre[0][0],deep,label_reference,arbre[0][1],arbre[0][3],arbre[0][2])
-        if not product_root:
-            product_root=product    
-        for i in range(len(arbre)-1):
-            product.links.append(generate_link(arbre[i+1],deep,product_root))
-                 
-    return product
+
+
+          
+    if not product_root:
+        product=generate_product(arbre,deep)
+        product_root=product
+    elif from_child_ArbreFile:
+        product=generate_product(arbre,deep)
+        product_assembly=search_assembly(product.name,label_reference,product.doc_id,product_root,product.geometry)
+        if product_assembly: 
+            from_child_ArbreFile.links.append(Link(product_assembly))
+            return False 
+        else:
+            from_child_ArbreFile.links.append(Link(product)) 
+          
+                    
+    for i in range(len(arbre)-1):
+        
+        product_child=generate_product(arbre[i+1][1],deep+1)
+        product_assembly=search_assembly(product_child.name,label_reference,product_child.doc_id,product_root,product_child.geometry)
+
+           
+        if product_assembly:
+            product_child=product_assembly 
+                                    
+        generate_link(arbre[i+1],product,product_child) 
+               
+        if not product_assembly:
+            generateArbre(arbre[i+1][1],product_child,product_root,deep+1)  
+            
+           
+    return product 
 
 
         
-def generate_link(arbre,deep,product_root):
-  
-    product=generateArbre(arbre[1],(deep+1),product_root)
-    link=Link(product)
+def generate_link(arbre,product,product_child):
+    product.links.append(Link(product_child))
     for i in range(len(arbre[0])):
-        link.add_occurrence(arbre[0][i][0],Matrix_rotation(False,arbre[0][i][1]))     
-    return link
+        product.links[-1].add_occurrence(arbre[0][i][0],Matrix_rotation(arbre[0][i][1]))
+        
+                 
+def generate_product(arbre,deep):
+    label_reference=False
+    return Product(arbre[0][0],deep,label_reference,arbre[0][1],arbre[0][3],arbre[0][2])   
 
+
+
+
+def search_assembly(name,label,id,product_root,geometry=False): # 2 modos , con geometrias y sin geometrias, si hay labels o si no las hay
 
   
-
-def search_assembly(name,label,id,product_root):
     if product_root: 
         for link in product_root.links:
-            if name and link.product.name==name: # puede haber asembly sin nombre!
-                if link.product.label_reference==label and id==link.product.doc_id: 
-                    return link.product
-                else:
-                    pass
+
+                
+            if name and link.product.name==name:
+                            
+                if label:
+
+                    if link.product.label_reference==label:
+                        print "Encontrado : " , link.product.name 
+                        return link.product
+                    
+                elif id==link.product.doc_id and geometry==link.product.geometry:
+                    print "Encontrado : " , link.product.name 
+                    return link.product                                        
+
+                #else:
+                    #pass
                     #raise "FATAL ERROR , 2 assembly diferente shape , FATAL FATALTALTALTALTATLA"
                 
             else:
-                product=search_assembly(name,label,id,link.product)
+ 
+                product=search_assembly(name,label,id,link.product,geometry)
                 if product:
                     return product
 
-
     
-        
+def getMatrixFromLocation(Location):
+
+
+    m=Location.VectorialPart()
+    gp=m.Row(1)
+    x1=gp.X()           
+    x2=gp.Y()
+    x3=gp.Z()
+    x4=Location.Transforms()[0]
+    gp=m.Row(2)
+    y1=gp.X()          
+    y2=gp.Y()
+    y3=gp.Z()
+    y4=Location.Transforms()[1]
+    gp=m.Row(3)
+    z1=gp.X()         
+    z2=gp.Y()
+    z3=gp.Z()
+    z4=Location.Transforms()[2]   
+    return [x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4]     
+    
+            
 def data_for_product(product):
     output=[]
 
