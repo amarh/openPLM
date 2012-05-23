9dd0c9f0404d9ecaca1bb0fc33c41f289eb32f32
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-05-23 10:24:44 +0000

    oerp: add costs

--- trunk/openPLM/oerp/urls.py
+++ trunk/openPLM/oerp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('openPLM.oerp.views',
-        (r'^object/([^/]+)/([^/]+)/([^/]+)/ERP/(?:publish/)?$', 'erp_summary'),
-    )
+    (r'^object/([^/]+)/([^/]+)/([^/]+)/ERP/$', 'erp_summary'),
+    (r'^object/([^/]+)/([^/]+)/([^/]+)/ERP/publish/$', 'erp_summary', {"publish" : True}),
+    (r'^object/([^/]+)/([^/]+)/([^/]+)/ERP/update_cost/$', 'erp_summary', {"update" : True}),
+)
