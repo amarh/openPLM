b5b23e9b3ae67c3cc3fee067c57ff21e589cfcc3
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-05-29 10:24:40 +0000

    add a thumbnailer for SolidWorks files, new dependency : libgsf-bin

---
+++ trunk/openPLM/plmapp/thumbnailers/swthumbnailer.py
@@ -0,0 +1,45 @@
+"""
+This module adds a thumbnailer for SolidWorks file.
+
+How it works
+============
+
+A SolidWorks file is an `OLE`_ file. This file contains
+an entry named ``PreviewPNG`` (or :samp:`{Something-}PreviewPNG`)
+which contains the thumbnail as a PNG file.
+
+This thumnailer uses :command:`gsf` (from ``libgsf-bin``) to find the name
+of the entry and to extract its content.
+
+.. _OLE: http://en.wikipedia.org/wiki/Object_Linking_and_Embedding
+
+"""
+
+import re
+import subprocess
+
+from base import ThumbnailersManager
+
+def sw_thumbnailer(input_path, original_filename, output_path):
+    """
+    Thumbnailer that extracts a thumlbnail of a SolidWorks file.
+    """
+    out = subprocess.Popen(["gsf", "list", input_path], stdout=subprocess.PIPE).communicate()[0]
+    previews = re.findall("[\w/-]*PreviewPNG", out)
+    if not previews:
+        raise ValueError("No preview found")
+    done = False
+    for preview in previews:
+        with open(output_path, "wb") as png_file:
+            args = ["gsf", "cat", input_path, preview]
+            call = subprocess.call(args, stdout=png_file)
+            done = call == 0
+        if done:
+            break
+    return True
+
+#: Supported formats
+FORMATS = (".sldprt", ".sldasm", ".sldasm", ".slddrw", ".slddrt")
+
+for ext in FORMATS:
+    ThumbnailersManager.register(ext, sw_thumbnailer)
