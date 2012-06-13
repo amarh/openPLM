d93e09ef6b0c41fad830a20247e5816a220f8eda
zali <zali@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-06-13 15:26:34 +0000

    Template navigate: the search is now performed asynchronously

--- trunk/openPLM/plmapp/views/main.py
+++ trunk/openPLM/plmapp/views/main.py
@@ -2151,3 +2151,8 @@ def public(request, obj_type, obj_ref, obj_revi):
 
     return r2r("public.html", ctx, request)
 
+def async_search(request, obj_type, obj_ref, obj_revi):
+    """Perform search_request asynchronously"""
+    obj,ctx = get_generic_data(request)
+    ctx["navigate_bool"]=True
+    return r2r("render_search.html",ctx,request)
