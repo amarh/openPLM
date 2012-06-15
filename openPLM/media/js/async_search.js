24e54087fe16a55bff2093f82cb9a6c7b854b87d
zali <zali@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-06-15 08:29:38 +0000

    Asynchronous search : update link to search and create pages

--- openPLM/media/js/async_search.js
+++ openPLM/media/js/async_search.js
@@ -10,12 +10,26 @@ function update_results(msg){
         var span_link = $(link).next("span");
         $(link).append(span_link);
     });
+    
     /*var searchBox=$(res_div).parent()[0];
     var searchBox_class = $(searchBox).attr("class");
     $("#SearchBox").attr("class",searchBox_class);*/
     $("#SearchBox > div.Result").replaceWith(res_div);
     if($(res_div).attr("navigate")=="true"){
         init();
+    }
+    
+    //update link to search and create pages
+    var req_type = $("#search_id_type").val();
+    var req_q = $("#search_id_q").val();
+    if (req_q==""){
+        req_q="*";
+    }
+    var search_link = $("#DisplayBox").find("a[href^='/search']")[0];
+    $(search_link).attr("href","/search/?type="+req_type+"&q="+req_q);
+    if( req_type!="User"){
+        var create_link = $("#DisplayBox").find("a[href^='/object/create']")[0];
+        $(create_link).attr("href","/object/create/?type="+req_type);
     }
 }
 
