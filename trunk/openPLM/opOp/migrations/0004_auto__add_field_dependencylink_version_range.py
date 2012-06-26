0e22103b935ddf3c13f64e0a6c73428f650cc77b
zali <zali@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-06-26 09:21:26 +0000

    add opOp (OpenPLM for OpenPLM) application

---
+++ trunk/openPLM/opOp/migrations/0004_auto__add_field_dependencylink_version_range.py
@@ -0,0 +1,154 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'DependencyLink.version_range'
+        db.add_column('opOp_dependencylink', 'version_range', self.gf('django.db.models.fields.CharField')(default=' ', max_length=50), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'DependencyLink.version_range'
+        db.delete_column('opOp_dependencylink', 'version_range')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'opOp.dependencylink': {
+            'Meta': {'object_name': 'DependencyLink', '_ormbases': ['plmapp.ParentChildLinkExtension']},
+            'parentchildlinkextension_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.ParentChildLinkExtension']", 'unique': 'True', 'primary_key': 'True'}),
+            'required': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'version_range': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'opOp.documentation': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Documentation', '_ormbases': ['plmapp.Part']},
+            'part_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.Part']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'opOp.package': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Package', '_ormbases': ['plmapp.Part']},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'part_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.Part']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'opOp.software': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Software', '_ormbases': ['plmapp.Part']},
+            'bugtracker_uri': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'licence': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'linobject_developpement': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'part_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.Part']", 'unique': 'True', 'primary_key': 'True'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'opOp.specification': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Specification', '_ormbases': ['plmapp.Document']},
+            'document_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.Document']", 'unique': 'True', 'primary_key': 'True'}),
+            'spec_uri': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        'opOp.urldoc': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'URLDoc', '_ormbases': ['plmapp.Document']},
+            'document_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.Document']", 'unique': 'True', 'primary_key': 'True'}),
+            'location_uri': ('django.db.models.fields.CharField', [], {'default': "' '", 'max_length': '250'})
+        },
+        'plmapp.document': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Document', '_ormbases': ['plmapp.PLMObject']},
+            'plmobject_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.PLMObject']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'plmapp.groupinfo': {
+            'Meta': {'object_name': 'GroupInfo', '_ormbases': ['auth.Group']},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'groupinfo_creator'", 'to': "orm['auth.User']"}),
+            'ctime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 6, 25, 17, 16, 15, 329798)'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'group_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.Group']", 'unique': 'True', 'primary_key': 'True'}),
+            'mtime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'groupinfo_owner'", 'to': "orm['auth.User']"})
+        },
+        'plmapp.lifecycle': {
+            'Meta': {'object_name': 'Lifecycle'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'primary_key': 'True'}),
+            'official_state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['plmapp.State']"})
+        },
+        'plmapp.parentchildlink': {
+            'Meta': {'unique_together': "(('parent', 'child', 'end_time'),)", 'object_name': 'ParentChildLink'},
+            'child': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'parentchildlink_child'", 'to': "orm['plmapp.Part']"}),
+            'ctime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'parentchildlink_parent'", 'to': "orm['plmapp.Part']"}),
+            'quantity': ('django.db.models.fields.FloatField', [], {'default': '1'}),
+            'unit': ('django.db.models.fields.CharField', [], {'default': "'-'", 'max_length': '4'})
+        },
+        'plmapp.parentchildlinkextension': {
+            'Meta': {'object_name': 'ParentChildLinkExtension'},
+            '_child_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'parentchildlinkextension_link'", 'to': "orm['plmapp.ParentChildLink']"})
+        },
+        'plmapp.part': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'object_name': 'Part', '_ormbases': ['plmapp.PLMObject']},
+            'plmobject_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['plmapp.PLMObject']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'plmapp.plmobject': {
+            'Meta': {'ordering': "['type', 'reference', 'revision']", 'unique_together': "(('reference', 'type', 'revision'),)", 'object_name': 'PLMObject'},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'plmobject_creator'", 'to': "orm['auth.User']"}),
+            'ctime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 6, 25, 17, 16, 15, 330768)'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'plmobject_group'", 'to': "orm['plmapp.GroupInfo']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lifecycle': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'plmobject_lifecyle'", 'to': "orm['plmapp.Lifecycle']"}),
+            'mtime': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'plmobject_owner'", 'to': "orm['auth.User']"}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'reference': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'reference_number': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'revision': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'plmobject_lifecyle'", 'to': "orm['plmapp.State']"}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'plmapp.state': {
+            'Meta': {'object_name': 'State'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['opOp']
