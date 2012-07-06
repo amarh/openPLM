b8985d720aba441adc936b826db62da2b6d091ab
zali <zali@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-07-06 15:19:25 +0000

    update translation script

--- trunk/openPLM/bin/translate_all.sh
+++ trunk/openPLM/bin/translate_all.sh
@@ -2,22 +2,49 @@
 
 # run this script to compile translations of all installed apps
 
+MAKEMESSAGES="django-admin makemessages"
+COMPILEMESSAGES="django-admin compilemessages"
 current=$(pwd)
 
-if [ -d "apps" ] && [ -d "locale" ];then
-    COMPILEMESSAGES="django-admin compilemessages"
-    APPS=$(ls apps)
-    
-    $COMPILEMESSAGES
-    for app in $APPS; do 
-        path="apps/$app"
 
-        # test if the locale directory for translations exists
-        if [ -d "$path/locale" ]; then
-            cd $path
+if [ -d "apps" ] && [ -d "locale" ];then
+    case $1 in
+    "compile")
+        if [ $# -eq "2" ] ; then
+            if [ $2 = "all" ];then
+                $COMPILEMESSAGES
+    
+                APPS=$(ls apps)
+                cd apps
+                for app in $APPS; do 
+                    if [ -d "$app/locale" ]; then
+                        cd $app
+                        $COMPILEMESSAGES
+                        cd ".."
+                    fi
+                done
+            else
+                if [ -d "apps/$2" ]; then
+                    cd "apps/$2"
+                    $COMPILEMESSAGES
+                else
+                    echo "$2 : this is not an app repertory"
+                fi
+            fi
+        else
             $COMPILEMESSAGES
+        fi;;
+    "make")
+        if [ $# -eq "2" ]; then
+            cd "apps/$2"
         fi
-    done
+        $MAKEMESSAGES -l fr
+        $MAKEMESSAGES -l es
+        $MAKEMESSAGES -l ja
+        $MAKEMESSAGES -l ru
+        $MAKEMESSAGES -l zh_CN
+    esac
 else
     echo "$current : this script should be run from openPLM directory"
 fi
+
