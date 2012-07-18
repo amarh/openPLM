3006a811929d850d5762edbfee35275c9e33b5ac
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-07-18 14:29:25 +0000

    settings: add messages template processor

--- trunk/openPLM/settings.py
+++ trunk/openPLM/settings.py
@@ -162,6 +162,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
         "django.core.context_processors.i18n",
         "django.core.context_processors.media",
         "django.core.context_processors.request",
+        "django.contrib.messages.context_processors.messages",
         )
 
 
