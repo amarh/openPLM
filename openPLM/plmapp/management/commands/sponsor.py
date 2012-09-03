60584c605e53a7da301f1197e1937ee6efb1e289
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-09-03 08:53:32 +0000

    add a command to sponsor a new user

---
+++ openPLM/plmapp/management/commands/sponsor.py
@@ -0,0 +1,61 @@
+"""
+Management utility to sponsor a new user.
+"""
+
+import getpass
+import os
+import re
+import sys
+from optparse import make_option
+from django.contrib.auth.models import User
+from django.core import exceptions
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+from django.utils import importlib
+
+from openPLM.plmapp.forms import SponsorForm
+from openPLM.plmapp.models import GroupInfo
+from openPLM.plmapp.controllers import UserController
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--sponsor', default=None,
+            help='Specifies the sponsor.'),
+        make_option('--username', default=None,
+            help='Specifies the username.'),
+        make_option('--role', default=SponsorForm.ROLES[0][0],
+            choices=[r[0] for r in SponsorForm.ROLES],
+            help='Specifies the user role.'),
+        make_option('--email', default=None,
+            help='Specifies the email.'),
+        make_option('--groups', default=None, action="append",
+            help='Specifies the groups.'),
+        make_option('--first_name', default=None,
+            help='Specifies the first name.'),
+        make_option('--last_name', default=None,
+            help='Specifies the last name.'),
+        make_option('--language', default='en',
+            help='Specifies the language.'),
+
+    )
+
+    args = '--sponsor=<username> --username=<new_username> --email=<e@e.net> --groups=<g1> --groups=<g2> --first_name=<fn> --last_name=<ln>'
+    help = 'Used to sponsor a new user'
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+        sponsor = User.objects.get(username=options.get('sponsor', None))
+        obj = UserController(sponsor, sponsor)
+        data = dict(options)
+        data["sponsor"] = sponsor.id
+        data["warned"] = True
+        data["groups"] = list(GroupInfo.objects.filter(name__in=options["groups"]).values_list("id", flat=True))
+        form = SponsorForm(data)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.get_profile().language = form.cleaned_data["language"]
+            role = form.cleaned_data["role"]
+            obj.sponsor(new_user, role=="contributor", role=="restricted")
+        else:
+            raise CommandError(form.errors.as_text())
+
