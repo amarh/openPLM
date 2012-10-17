741b6a07dc3bea85738d5e74ae1ba1e5a226a839
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-10-17 13:59:37 +0000

    document3D: update three.js to r52

--- trunk/openPLM/media/js/3D/view.js
+++ trunk/openPLM/media/js/3D/view.js
@@ -1,4 +1,55 @@
+/**
+* old AxisHelper
+* @author sroucheray / http://sroucheray.org/
+* @author mrdoob / http://mrdoob.com/
+*/
 
+THREE.AxisHelper = function () {
+
+THREE.Object3D.call( this );
+
+var lineGeometry = new THREE.Geometry();
+lineGeometry.vertices.push( new THREE.Vector3() );
+lineGeometry.vertices.push( new THREE.Vector3( 0, 100, 0 ) );
+
+var coneGeometry = new THREE.CylinderGeometry( 0, 5, 25, 5, 1 );
+
+var line, cone;
+
+// x
+
+line = new THREE.Line( lineGeometry, new THREE.LineBasicMaterial( { color : 0xff0000 } ) );
+line.rotation.z = - Math.PI / 2;
+this.add( line );
+
+cone = new THREE.Mesh( coneGeometry, new THREE.MeshBasicMaterial( { color : 0xff0000 } ) );
+cone.position.x = 100;
+cone.rotation.z = - Math.PI / 2;
+this.add( cone );
+
+// y
+
+line = new THREE.Line( lineGeometry, new THREE.LineBasicMaterial( { color : 0x00ff00 } ) );
+this.add( line );
+
+cone = new THREE.Mesh( coneGeometry, new THREE.MeshBasicMaterial( { color : 0x00ff00 } ) );
+cone.position.y = 100;
+this.add( cone );
+
+// z
+
+line = new THREE.Line( lineGeometry, new THREE.LineBasicMaterial( { color : 0x0000ff } ) );
+line.rotation.x = Math.PI / 2;
+this.add( line );
+
+cone = new THREE.Mesh( coneGeometry, new THREE.MeshBasicMaterial( { color : 0x0000ff } ) );
+cone.position.z = 100;
+cone.rotation.x = Math.PI / 2;
+this.add( cone );
+
+};
+
+THREE.AxisHelper.prototype = Object.create( THREE.Object3D.prototype );
 $(function() {
     if ( !window.requestAnimationFrame ) {
 
@@ -424,7 +475,7 @@ View3D.prototype = {
         var rotObjectMatrix = new THREE.Matrix4();
         rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
         object.matrix.multiplySelf(rotObjectMatrix);      // post-multiply
-        object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+        object.rotation.setEulerFromRotationMatrix(object.matrix);
     },
 
     view_rear : function (){
