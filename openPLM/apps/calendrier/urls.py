4e4e528add53e2808a5c12fe9d7b6beed9c4c7fb
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-11-19 16:36:45 +0000

    calendrier: adds a link to download calendar in icalendar format if django_ical is installed

--- openPLM/apps/calendrier/urls.py
+++ openPLM/apps/calendrier/urls.py
@@ -7,7 +7,7 @@ object_url = r'^object/' + object_pattern
 user_url = r'^user/(?P<obj_ref>[^/]+)/'
 group_url = r'^group/(?P<obj_ref>[^/]+)/'
 date_url = r'calendar/(?:(?P<year>\d{1,4})/(?:(?P<month>\d\d?)/)?)?$'
-
+ics_url = date_url[:-1] + "ics/$"
 
 urlpatterns = patterns('',
     (r'^timeline/' + date_url, views.history_calendar, {"timeline" : True}),
@@ -16,8 +16,18 @@ urlpatterns = patterns('',
     (group_url + r'history/' + date_url, views.history_calendar, {"obj_type" : "Group"}),
 
     
-    ('^timeline/', views.history, {"timeline" : True,} ),
-    (object_url + r'history/', views.history, ),
-    (user_url + r'history/', views.history, {"obj_type" : "User" }),
-    (group_url + r'history/', views.history, { "obj_type" : "Group" }),
+    ('^timeline/$', views.history, {"timeline" : True,} ),
+    (object_url + r'history/$', views.history, ),
+    (user_url + r'history/$', views.history, {"obj_type" : "User" }),
+    (group_url + r'history/$', views.history, { "obj_type" : "Group" }),
     )
+
+if views.ICAL_INSTALLED:
+    urlpatterns += patterns('',
+        ('^timeline/' + ics_url , views.TimelineCalendarFeed()),
+        (object_url + r'history/' + ics_url, views.CalendarFeed()),
+        (user_url + r'history/' + ics_url, views.CalendarFeed(), {"obj_type" : "User", "obj_revi" : "-"}),
+        (group_url + r'history/' + ics_url, views.CalendarFeed(), { "obj_type" : "Group",
+            "obj_revi" : "-"}),
+    )
+
