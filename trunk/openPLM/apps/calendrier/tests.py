56521cfb1aafbb73d98e5679cc996153597fd520
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-11-20 13:34:45 +0000

    calendrier: more tests

--- trunk/openPLM/apps/calendrier/tests.py
+++ trunk/openPLM/apps/calendrier/tests.py
@@ -1,23 +1,52 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from openPLM.plmapp.tests.views import CommonViewTest
 
-Replace these with more appropriate tests for your application.
-"""
+class CalendarTestCase(CommonViewTest):
 
-from django.test import TestCase
+    def test_history(self):
+        " Assert that the template ``history_cal.html`` is used."
+        response = self.get(self.base_url + "history/")
+        self.assertTemplateUsed(response, "history_cal.html")
+        # history_cal.html inherits from history.html
+        self.assertTemplateUsed(response, "history.html")
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    def test_calendar_part(self):
+        response = self.get(self.base_url + "history/calendar/")
+        self.assertEqual("", response.context["prefix_url"])
+        calendar = response.context["calendar"]
+        # TODO: check calendar
+        # checks months
+        previous_month = response.context["previous_month"]
+        current_month = response.context["current_month"]
+        next_month = response.context["next_month"]
+        self.assertTrue(previous_month < current_month < next_month)
+        self.assertNotEqual(previous_month.month, current_month.month)
+        self.assertNotEqual(next_month.month, current_month.month)
+        # get calendar of next month
+        response_next = self.get(self.base_url + "history/calendar/%d/%d/" % 
+                (next_month.year, next_month.month))
+        self.assertEqual("../../", response_next.context["prefix_url"])
+        calendar_next = response_next.context["calendar"]
+        self.assertNotEqual(calendar, calendar_next)
+        self.assertEqual(current_month.year, response_next.context["previous_month"].year)
+        self.assertEqual(current_month.month, response_next.context["previous_month"].month)
+        # get calendar of previous month
+        response_previous = self.get(self.base_url + "history/calendar/%d/%d/" % 
+                (previous_month.year, previous_month.month))
+        self.assertEqual("../../", response_previous.context["prefix_url"])
+        calendar_previous = response_previous.context["calendar"]
+        self.assertNotEqual(calendar, calendar_previous)
+        self.assertEqual(current_month.year, response_previous.context["next_month"].year)
+        self.assertEqual(current_month.month, response_previous.context["next_month"].month)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_calendar_user(self):
+        response = self.get("/user/%s/history/calendar/" % self.user.username)
+        calendar = response.context["calendar"]
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_calendar_group(self):
+        response = self.get("/group/%s/history/calendar/" % self.group.name)
+        calendar = response.context["calendar"]
+
+    def test_calendar_timeline(self):
+        response = self.get("/timeline/calendar/")
+        calendar = response.context["calendar"]
 
