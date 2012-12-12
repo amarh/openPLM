fce45a531a1040d872d440db7cd89007343b8add
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-12-12 16:57:13 +0000

    add iobject interface will be implmeented by EngineeringRequestRequest

---
+++ openPLM/plmapp/models/iobject.py
@@ -0,0 +1,30 @@
+class IObject(object):
+
+    @property
+    def plmobject_url(self):
+        return u""
+    
+    @property
+    def menu_items(self):
+        "menu items to choose a view"
+        return []
+
+    @property
+    def attributes(self):
+        u"Attributes to display in `Attributes view`"
+        return []
+
+    @classmethod
+    def get_creation_fields(cls):
+        """
+        Returns fields which should be displayed in a creation form.
+        """
+        return []
+
+    @classmethod
+    def get_modification_fields(cls):
+        """
+        Returns fields which should be displayed in a modification form
+        """
+        return []
+
