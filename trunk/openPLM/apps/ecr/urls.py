5344d78326024ad70de780be001337ebc73891d0
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-12-14 13:56:40 +0000

    ecr: add management urls

--- trunk/openPLM/apps/ecr/urls.py
+++ trunk/openPLM/apps/ecr/urls.py
@@ -9,8 +9,14 @@ ecr = r'^ecr/(?P<obj_ref>%(x)s)/' % {'x' : r'[^/?#\t\r\v\f]+'}
 ecr_dict = {"obj_type" : "ECR", "obj_revi" : "-"}
 
 urlpatterns = patterns('',
-        (ecr + "(?:attributes/)?$", pviews.display_object_attributes, ecr_dict ),
-        (ecr + "history/$", pviews.display_object_history, ecr_dict ),
-        (ecr + "lifecycle/$", pviews.display_object_lifecycle, ecr_dict ),
-        (ecr + "lifecycle/apply/$", pviews.display_object_lifecycle, ecr_dict ),
-                      )
+    (ecr + "(?:attributes/)?$", pviews.display_object_attributes, ecr_dict ),
+    (ecr + "history/$", pviews.display_object_history, ecr_dict ),
+    (ecr + "lifecycle/$", pviews.display_object_lifecycle, ecr_dict ),
+    (ecr + "lifecycle/apply/$", pviews.display_object_lifecycle, ecr_dict ),
+    (ecr + 'management/add/$', pviews.add_management, ecr_dict),
+    (ecr + 'management/add-reader/$', pviews.add_management, dict(obj_type="ECR",
+        obj_revi="-", reader=True)),
+    (ecr + 'management/add-signer(?P<level>\d+)/$', pviews.add_management, ecr_dict),
+    (ecr + 'management/replace/(?P<link_id>\d+)/$', pviews.replace_management, ecr_dict),
+    (ecr + 'management/delete/$', pviews.delete_management, ecr_dict),
+    )
