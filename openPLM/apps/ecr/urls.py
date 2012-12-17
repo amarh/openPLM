fb4bacf4b86c9d9706525cc9f07a5b2e8620ef99
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2012-12-17 10:10:04 +0000

    ecr: browse

--- openPLM/apps/ecr/urls.py
+++ openPLM/apps/ecr/urls.py
@@ -4,19 +4,20 @@ import openPLM.apps.ecr.views as views
 import openPLM.apps.ecr.controllers
 import openPLM.plmapp.views as pviews
 
-ecr = r'^ecr/(?P<obj_ref>%(x)s)/' % {'x' : r'[^/?#\t\r\v\f]+'}
+ecr = r'^ecr/(?P<obj_ref>%(x)s)/' % {'x': r'[^/?#\t\r\v\f]+'}
 
-ecr_dict = {"obj_type" : "ECR", "obj_revi" : "-"}
+ecr_dict = {"obj_type": "ECR", "obj_revi": "-"}
 
 urlpatterns = patterns('',
-    (ecr + "(?:attributes/)?$", pviews.display_object_attributes, ecr_dict ),
-    (ecr + "history/$", pviews.display_object_history, ecr_dict ),
-    (ecr + "lifecycle/$", pviews.display_object_lifecycle, ecr_dict ),
-    (ecr + "lifecycle/apply/$", pviews.display_object_lifecycle, ecr_dict ),
+    (ecr + "(?:attributes/)?$", pviews.display_object_attributes, ecr_dict),
+    (ecr + "history/$", pviews.display_object_history, ecr_dict),
+    (ecr + "lifecycle/$", pviews.display_object_lifecycle, ecr_dict),
+    (ecr + "lifecycle/apply/$", pviews.display_object_lifecycle, ecr_dict),
     (ecr + 'management/add/$', pviews.add_management, ecr_dict),
     (ecr + 'management/add-reader/$', pviews.add_management, dict(obj_type="ECR",
-        obj_revi="-", reader=True)),
+                                                                obj_revi="-", reader=True)),
     (ecr + 'management/add-signer(?P<level>\d+)/$', pviews.add_management, ecr_dict),
     (ecr + 'management/replace/(?P<link_id>\d+)/$', pviews.replace_management, ecr_dict),
     (ecr + 'management/delete/$', pviews.delete_management, ecr_dict),
+    ('browse/ecr/$', views.browse_ecr),
     )
--- openPLM/apps/ecr/views.py
+++ openPLM/apps/ecr/views.py
@@ -1,15 +1,58 @@
-from openPLM.plmapp.base_views import handle_errors, register_creation_view
-from openPLM.plmapp.views import create_object
+from django.db.models import F, Q
+from django.utils.translation import ugettext_lazy as _
+
+from openPLM.plmapp import models
+import openPLM.plmapp.base_views as bv
+from openPLM.plmapp.views import create_object, r2r, get_pagination
 
 from openPLM.apps.ecr.forms import get_creation_form
 from openPLM.apps.ecr.models import ECR
 
-@handle_errors
+
+@bv.handle_errors
 def create_ecr(request, *args):
     if request.method == 'GET':
         creation_form = get_creation_form(request.user)
     elif request.method == 'POST':
         creation_form = get_creation_form(request.user, request.POST)
     return create_object(request, True, creation_form)
-register_creation_view(ECR, create_ecr)
+bv.register_creation_view(ECR, create_ecr)
 
+
+@bv.secure_required
+def browse_ecr(request):
+    user = request.user
+    if user.is_authenticated() and not user.get_profile().restricted:
+        # only authenticated users can see all groups and users
+        obj, ctx = bv.get_generic_data(request, search=False)
+        object_list = ECR.objects.all()
+        # this is only relevant for authenticated users
+        ctx["state"] = state = request.GET.get("state", "all")
+        if state == "official":
+            object_list = object_list.\
+                exclude(lifecycle=models.get_cancelled_lifecycle()).\
+                filter(state=F("lifecycle__official_state"))
+        ctx["plmobjects"] = False
+    else:
+        ctx = bv.init_ctx("-", "-", "-")
+        ctx.update({
+            'is_readable': True,
+            'restricted': True,
+            'object_menu': [],
+            'navigation_history': [],
+        })
+        query = Q(published=True)
+        if user.is_authenticated():
+            readable = user.ecruserlink_user.now().filter(role=models.ROLE_READER)
+            readable |= user.ecruserlink_user.now().filter(role=models.ROLE_OWNER)
+            query |= Q(id__in=readable.values_list("ecr_id", flat=True))
+        object_list = ECR.objects.filter(query)
+
+    ctx.update(get_pagination(request.GET, object_list, type))
+    extra_types = [c.__name__ for c in models.IObject.__subclasses__()]
+    ctx.update({
+        "object_type": _("Browse"),
+        "type": "ECR",
+        "extra_types": extra_types,
+    })
+    return r2r("browse_ecr.html", ctx, request)
