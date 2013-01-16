221a6f971975ef1d85ae4a30ccab33b7ad4f7c17
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-01-16 09:05:02 +0000

    docs: s/2012/2012/

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'openPLM'
-copyright = u'2010-2012, LinObject'
+copyright = u'2010-2013, LinObject'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
