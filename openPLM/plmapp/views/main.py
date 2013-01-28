5b81cd015a6e3e74a5e136fec72a421844fa88a9
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-01-28 11:25:42 +0000

    browse: exclude cancelled objects, see #184

--- openPLM/plmapp/views/main.py
+++ openPLM/plmapp/views/main.py
@@ -2865,10 +2865,9 @@ def browse(request, type="object"):
         ctx["state"] = state = request.GET.get("state", "all")
         if type in ("object", "part", "topassembly", "document"):
             ctx["plmobjects"] = True
+            object_list = object_list.exclude(lifecycle=models.get_cancelled_lifecycle())
             if state == "official":
-                object_list = object_list.\
-                        exclude(lifecycle=models.get_cancelled_lifecycle()).\
-                        filter(state=F("lifecycle__official_state"))
+                object_list = object_list.filter(state=F("lifecycle__official_state"))
             elif state == "published":
                 object_list = object_list.filter(published=True)
         else:
@@ -2895,7 +2894,7 @@ def browse(request, type="object"):
         if user.is_authenticated():
             readable = user.plmobjectuserlink_user.now().filter(role=models.ROLE_READER)
             query |= Q(id__in=readable.values_list("plmobject_id", flat=True))
-        object_list = cls.filter(query)
+        object_list = cls.filter(query).exclude(lifecycle=models.get_cancelled_lifecycle())
 
     ctx.update(get_pagination(request.GET, object_list, type))
     extra_types = [c.__name__ for c in models.IObject.__subclasses__()]
