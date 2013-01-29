085c63d78f1c9aca72e3982c97966d655c24d247
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-01-29 10:31:19 +0000

    update application example

--- trunk/docs/en/devel/code/bicycle/views.py
+++ trunk/docs/en/devel/code/bicycle/views.py
@@ -1,15 +1,16 @@
-import openPLM.plmapp.views as pviews
+from openPLM.plmapp.base_views import get_generic_data
+from openPLM.plmapp.utils import r2r
 
 
 def attributes(request, obj_ref, obj_revi):
-    """ Manage html page for attributes """
+    """Custom attributes page """
     obj_type = "Bicycle"
-    obj, ctx = pviews.get_generic_data(request, obj_type, obj_ref, obj_revi)
+    obj, ctx = get_generic_data(request, obj_type, obj_ref, obj_revi)
     object_attributes = []
     for attr in obj.attributes:
         item = obj.get_verbose_name(attr) + ":" # <- this is our small modification
         object_attributes.append((item, getattr(obj, attr)))
-    ctx.update({'current_page':'attributes', 
+    ctx.update({'current_page':'attributes',
                 'object_attributes': object_attributes})
-    return pviews.r2r('attributes.html', ctx, request)
+    return r2r('attributes.html', ctx, request)
 
