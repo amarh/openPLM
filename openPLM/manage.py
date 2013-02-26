c52a7c876d53f1cf3133dc836608dc66e30113ec
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-02-26 09:12:00 +0000

    update manage.py, see #187

--- openPLM/manage.py
+++ openPLM/manage.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+import sys
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-from django.core.management import execute_manager
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openPLM.settings")
 
 try:
     import settings # Assumed to be in the same directory.
 except ImportError as er:
-    import sys
     # print 'er' in case of a missing module dependence
     # reported by Jason Morgan
     sys.stderr.write(str(er))
     sys.stderr.write("\nError: Can't find the file 'settings.py' in the directory\n")
     sys.exit(1)
 if __name__ == "__main__":
-    execute_manager(settings)
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
