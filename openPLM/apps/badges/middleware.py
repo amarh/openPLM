54058d8de788ec365aebe616609b553df90c8129
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-02-26 15:10:11 +0000

    badges: switch to the django.contrib.messages framework A special middleware must be added: `'openPLM.apps.badges.middleware.GlobalRequest'`

---
+++ openPLM/apps/badges/middleware.py
@@ -0,0 +1,28 @@
+# from http://djangosnippets.org/snippets/2853/ by myq
+
+from threading import current_thread
+
+class GlobalRequest(object):
+    _requests = {}
+
+    @staticmethod
+    def get_request():
+	try:
+            return GlobalRequest._requests[current_thread()]
+	except KeyError:
+            return None
+
+    def process_request(self, request):
+        GlobalRequest._requests[current_thread()] = request
+
+    def process_response(self, request, response):
+        # Cleanup
+        thread = current_thread()
+        try:
+            del GlobalRequest._requests[thread]
+        except KeyError:
+            pass
+        return response
+
+def get_request():
+    return GlobalRequest.get_request()
