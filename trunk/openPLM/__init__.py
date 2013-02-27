2cfadc6d0368c828e1adccd24e76aad5145092b9
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-02-27 08:29:05 +0000

    __init__.py : backport get_svn_revision from old django (django 1.5 switched to git)

--- trunk/openPLM/__init__.py
+++ trunk/openPLM/__init__.py
@@ -1,8 +1,46 @@
 import os.path
+import re
 
 VERSION = (1, 3, 0, 'dev', 0)
 
-# from django.__init__
+def get_svn_revision(path):
+    """
+    Returns the SVN revision in the form SVN-XXXX,
+    where XXXX is the revision number.
+
+    Returns SVN-unknown if anything goes wrong, such as an unexpected
+    format of internal SVN files.
+
+    *path* must be a directory whose SVN info you want to
+    inspect. If it's not provided, this will use the root django/ package
+    directory.
+    """
+    # from old djnago.utils.version package (< 1.5)
+    rev = None
+    entries_path = '%s/.svn/entries' % path
+
+    try:
+        entries = open(entries_path, 'r').read()
+    except IOError:
+        pass
+    else:
+        # Versions >= 7 of the entries file are flat text.  The first line is
+        # the version number. The next set of digits after 'dir' is the revision.
+        if re.match('(\d+)', entries):
+            rev_match = re.search('\d+\s+dir\s+(\d+)', entries)
+            if rev_match:
+                rev = rev_match.groups()[0]
+        # Older XML versions of the file specify revision as an attribute of
+        # the first entries node.
+        else:
+            from xml.dom import minidom
+            dom = minidom.parse(entries_path)
+            rev = dom.getElementsByTagName('entry')[0].getAttribute('revision')
+
+    if rev:
+        return u'SVN-%s' % rev
+    return u'SVN-unknown'
+
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
     if VERSION[2]:
@@ -12,7 +50,6 @@ def get_version():
     else:
         if VERSION[3] != 'final':
             version = '%s %s %s' % (version, VERSION[3], VERSION[4])
-    from django.utils.version import get_svn_revision
     svn_rev = get_svn_revision(os.path.dirname(__file__))
     if svn_rev != u'SVN-unknown':
         version = "%s %s" % (version, svn_rev)
