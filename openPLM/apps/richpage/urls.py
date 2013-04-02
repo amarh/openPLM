dcf4f148222b7df9e33ac11cb52eadc8b54f228a
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-04-02 10:02:07 +0000

    richpage: redirects "Page/ref/rev/" to the "page" tab instead of the "files" tab

--- openPLM/apps/richpage/urls.py
+++ openPLM/apps/richpage/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import *
+from django.views.generic import RedirectView
 
 import openPLM.apps.richpage.views
 
@@ -6,6 +7,8 @@ object_pattern = '(?P<obj_type>Page)/(?P<obj_ref>%(x)s)/(?P<obj_revi>%(x)s)/' % 
 
 object_url = r'^object/' + object_pattern
 urlpatterns = patterns('',
+    (object_url +'$', RedirectView.as_view(url="/object/%(obj_type)s/%(obj_ref)s/%(obj_revi)s/page/",
+     permanent=True)),
     (object_url + r'page/$', 'openPLM.apps.richpage.views.display_page'),
     (object_url + r'edit_content/$', 'openPLM.apps.richpage.views.edit_content'),
 )
