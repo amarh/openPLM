8cbdfb254ec54b75748d99805eb3fffc5f84f3b0
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-04-04 15:56:16 +0000

    part controller: add a promote_assembly method Not finished (see TODO and FIXME items)

--- trunk/openPLM/plmapp/tests/controllers/__init__.py
+++ trunk/openPLM/plmapp/tests/controllers/__init__.py
@@ -5,3 +5,4 @@ from openPLM.plmapp.tests.controllers.document import *
 from openPLM.plmapp.tests.controllers.deletion import *
 from openPLM.plmapp.tests.controllers.group import *
 from openPLM.plmapp.tests.controllers.partdoc import *
+from openPLM.plmapp.tests.controllers.assembly import *
