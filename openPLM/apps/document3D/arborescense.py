8581db2335b336e28e71d935dd56b77d974e2bb8
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-04-17 10:07:13 +0000

    document3D: 3D view: replace the part/shape selector with a more usable treeview

--- openPLM/apps/document3D/arborescense.py
+++ openPLM/apps/document3D/arborescense.py
@@ -1,3 +1,4 @@
+from django.utils.html import escape
 
 class JSGenerator(object):
 
@@ -13,20 +14,30 @@ function change_part%(counter)s(attr) {
     }
     else{
         part%(counter)s.visible=attr;
-    }       
-""" 
+    }
+    var span = $("#li-part-%(counter)s");
+    if (part%(counter)s.visible){
+        span.removeClass("part_hidden");
+    }
+    else {
+        span.addClass("part_hidden");
+        var li = span.parent();
+        li.addClass("expanded").removeClass("open");
+        li.children("ul").hide();
+    }
+"""
 
     _function_change_part = """
-    change_part%(child_counter)s(part%(counter)s.visible); 
+    change_part%(child_counter)s(part%(counter)s.visible);
     """
     _function_change_object = """
-    object%(object_counter)s.visible=part%(counter)s.visible;   
-    """                                    
- 
+    object%(object_counter)s.visible=part%(counter)s.visible;
+    """
+
     _menu_tpl = """
 function menu() {
-    element = document.createElement("div");
-    element.id="root";
+    element = document.createElement("ul");
+    element.id="tree";
     element.innerHTML ="%s</li></ul>";
     document.getElementById("menu_").appendChild(element);
 }
@@ -41,10 +52,10 @@ function menu() {
     def get_js(self):
         if not self.product:
             return ""
-        self.menu_items.append(self._get_menu_item(0, self.product.name))
+        self.menu_items.append(self._get_menu_item(0, self.product.name, self.product.geometry))
         self._process(self.product, [], self.counter)
         return self._header + self._get_menu() + "".join(self.js)
-    
+
     def _process(self, product, locations, old_counter):
         self.counter += 1
         self.menu_items.append("<ul>")
@@ -57,7 +68,8 @@ function menu() {
                     loc2 = locations[:]
                     loc2.append(link.locations[i])
                     generated_parts.append(self.counter)
-                    self.menu_items.append(self._get_menu_item(self.counter, link.names[i]))
+                    self.menu_items.append(self._get_menu_item(self.counter, link.names[i],
+                        link.product.geometry))
                     self._process(link.product, loc2, self.counter)
                     self.menu_items.append("</li>")
             self._add_parts(old_counter, generated_parts)
@@ -79,13 +91,14 @@ function menu() {
         self.js.extend(parts_definition)
         self.js.extend(function)
         self.js.extend(part_to_parts)
-     
+
     def _add_object3d(self, counter, object_counter, product, loc):
         reference = product.geometry
-        part_id = str(product.doc_id)        
-        js = self.js                     
-        function = ((self._function_head + self._function_change_object 
-                + "}\npart_to_object['part%(counter)s'] = object%(object_counter)s;") % locals())
+        part_id = str(product.doc_id)
+        js = self.js
+        function = ((self._function_head + self._function_change_object
+                + "}\npart_to_object['part%(counter)s'] = object%(object_counter)s;\n"
+                + "object%(object_counter)s.part = '%(counter)s';\n") % locals())
 
         counter = object_counter
         js.append("var object%s=new THREE.Mesh(_%s_%s,material_for_%s_%s );\n"%(counter, reference,
@@ -98,11 +111,17 @@ object%s.matrix.multiplySelf(new THREE.Matrix4(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
                             l.y1, l.y2, l.y3, l.y4,
                             l.z1, l.z2, l.z3, l.z4))
         js.append("object3D.add(object%s);\n" % counter)
-        js.append(function)        
-           
-    def _get_menu_item(self, counter,name):
+        js.append(function)
 
-        onclick="change_part"+str(counter)+"(\\\"click\\\")" 
-        return "<li > <a href='#' id='li-part-%s' onClick='%s'><b onClick='%s'></b>%s </a>" % (counter, onclick,onclick,name)
-             
+    def _get_menu_item(self, counter, name, is_child):
+        onclick = 'change_part%d(\\"click\\");' % counter
+        name = escape(name)
+        if is_child:
+            li = ("<li><span class='part' id='li-part-{counter}' onClick='{onclick}'>{name}</span>&nbsp;"
+            "<span id='color-{counter}'>&nbsp;&nbsp;</span>")
+        else:
+            li = ("<li class='open expander'><span class='expander'></span>"
+                    "<span class='part' id='li-part-{counter}' onClick='{onclick}'>{name}</span>")
+        return li.format(counter=counter, name=name, onclick=onclick)
+
 
