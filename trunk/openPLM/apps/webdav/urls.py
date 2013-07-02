991b8e9e4c6123beea0b491c08c1989fb40253e4
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-07-02 15:32:06 +0000

    webdav: AutoRun.inf is another file the windows client may try to open

--- trunk/openPLM/apps/webdav/urls.py
+++ trunk/openPLM/apps/webdav/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # workaround against dummy queries from windows clients
-    url(r'^dav/(?:[^/]+/)*(?:desktop.inii|Thumbs.db|folder.(?:jpg|gif))$', 'openPLM.apps.webdav.views.not_found'),
+    url(r'^dav/(?:[^/]+/)*(?:[dD]esktop.ini|AutoRun.inf|Thumbs.db|folder.(?:jpg|gif))$', 'openPLM.apps.webdav.views.not_found'),
 
     url(r'^dav/(?P<local_path>.*)$', 'openPLM.apps.webdav.views.openplm_webdav'),
 )
