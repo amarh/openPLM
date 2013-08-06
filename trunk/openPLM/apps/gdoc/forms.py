884dc3aa8bf2b69ea43aae88a37cc527aeca6b8f
pcosquer <pcosquer@5b46f505-65de-4892-aab2-a53e26d394e5>
2013-08-06 08:38:57 +0000

    creation form: reorder attributes:
        1. name
        2. group
        3. specific attributes (supplier, material...)
        4. reference
        5. revision
        6. lifecycle
        7. description
    
    TODO: add a hide/show bar 
    
    see #212

--- trunk/openPLM/apps/gdoc/forms.py
+++ trunk/openPLM/apps/gdoc/forms.py
@@ -10,7 +10,7 @@ from openPLM.apps.gdoc.gutils import get_gdocs
 def clean_gdoc(self):
     cleaned_data = self.cleaned_data
     cleaned_data["name"] = self.gdocs[cleaned_data["resource_id"]]
-    return cleaned_data 
+    return cleaned_data
 
 def get_gdoc_creation_form(user, client, data=None, start=0):
     form = get_creation_form(user, GoogleDocument, data, start)
@@ -18,6 +18,8 @@ def get_gdoc_creation_form(user, client, data=None, start=0):
     form.gdocs = dict(gdocs)
     form.fields["resource_id"] = forms.ChoiceField(choices=gdocs,
             required=True)
+    del form.fields.keyOrder[-1]
+    form.fields.keyOrder.insert(0, "resource_id")
     old_clean = form.clean
     form.clean = lambda: old_clean() and clean_gdoc(form)
     return form
